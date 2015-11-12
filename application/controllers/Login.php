110d540cb4a3fb6522dd801e1c37e4305b4fd0dc
Rxs84217621 <84217621@qq.com>
2015-11-12 21:32:01 +0800

    update

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,11 +5,33 @@ class Login extends CI_Controller {
 	/**
 	 * Index Page for this controller.
 	 */
+	function _construct(){
+
+
+		paent::_construct();
+
+	}
 	public function index()
 	{
 		// $this->load->helper('url');
 		$this->load->view("login.html");
 	}
+	//登录方法
+function  checklogin(){
+		//获得表单的用户名和密码
+		$user_name=$this->input->post('uName');
+		$user_pwd=$this->input->post('uPwd')
+		$type=1;
+
+
+
+
+
+}
+
+
+
+
 }
 
 /* End of file welcome.php */
