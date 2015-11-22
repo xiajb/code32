3aabf546eee85e6df228762ab9b958e2d957682b
tanxu <84217621@qq.com>
2015-11-22 19:16:26 +0800

    add

--- application/controllers/admin/Main.php
+++ application/controllers/admin/Main.php
@@ -7,27 +7,27 @@ class Main extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('admin_header.html');
-		$this->load->view('admin_user.html');
-		$this->load->view('admin_footer.html');
+		$this->load->view('admin/admin_header.html');
+		$this->load->view('admin/admin_user.html');
+		$this->load->view('admin/admin_footer.html');
 	}
 	public function teacher()
 	{
-		$this->load->view('admin_header.html');
-		$this->load->view('admin_teacher.html');
-		$this->load->view('admin_footer.html');
+		$this->load->view('admin/admin_header.html');
+		$this->load->view('admin/admin_teacher.html');
+		$this->load->view('admin/admin_footer.html');
 	}
 	public function course()
 	{
-		$this->load->view('admin_header.html');
-		$this->load->view('admin_course.html');
-		$this->load->view('admin_footer.html');
+		$this->load->view('admin/admin_header.html');
+		$this->load->view('admin/admin_course.html');
+		$this->load->view('admin/admin_footer.html');
 	}
 	public function video()
 	{
-		$this->load->view('admin_header.html');
-		$this->load->view('admin_upload.html');
-		$this->load->view('admin_footer.html');
+		$this->load->view('admin/admin_header.html');
+		$this->load->view('admin/admin_upload.html');
+		$this->load->view('admin/admin_footer.html');
 	}
 }
 
