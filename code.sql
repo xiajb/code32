7e8430e34be7ec09bb293ffda7f69d7066354e0c
tanxu <773184719@qq.com>
2016-01-11 15:25:03 +0800

    add

--- code.sql
+++ code.sql
@@ -38,11 +38,11 @@ CREATE TABLE IF NOT EXISTS `ci_chapter` (
 -- 转存表中的数据 `ci_chapter`
 --
 
-INSERT INTO `ci_chapter` (`chapter_id`, `chapter_name`, `course_id`, `order_no`) VALUES
-(1, 'HTML入门', 1, 1),
-(2, 'JavaScript入门', 1, 2),
-(3, 'Html介绍', 2, 1),
-(4, '认识标签(第一部分)', 2, 2);
+-- INSERT INTO `ci_chapter` (`chapter_id`, `chapter_name`, `course_id`, `order_no`) VALUES
+-- (1, 'HTML入门', 1, 1),
+-- (2, 'JavaScript入门', 1, 2),
+-- (3, 'Html介绍', 2, 1),
+-- (4, '认识标签(第一部分)', 2, 2);
 
 -- --------------------------------------------------------
 
@@ -61,26 +61,26 @@ CREATE TABLE IF NOT EXISTS `ci_classify` (
 -- 转存表中的数据 `ci_classify`
 --
 
-INSERT INTO `ci_classify` (`classify_id`, `classify_name`, `direction_id`) VALUES
-(1, 'HTML/CSS', 1),
-(2, 'JavaScript', 1),
-(3, 'CSS3', 1),
-(4, 'Html5', 1),
-(5, 'jQuery', 1),
-(6, 'AngularJS', 1),
-(7, 'Node.js', 1),
-(8, 'Bootstrap', 1),
-(9, 'WebApp', 1),
-(10, '前端工具', 1),
-(11, 'PHP', 2),
-(12, 'JAVA', 2),
-(13, 'Linux', 2),
-(14, 'Python', 2),
-(15, 'C', 2),
-(16, 'C++', 2),
-(17, 'Go', 2),
-(18, 'C#', 2),
-(19, '数据结构', 2);
+-- INSERT INTO `ci_classify` (`classify_id`, `classify_name`, `direction_id`) VALUES
+-- (1, 'HTML/CSS', 1),
+-- (2, 'JavaScript', 1),
+-- (3, 'CSS3', 1),
+-- (4, 'Html5', 1),
+-- (5, 'jQuery', 1),
+-- (6, 'AngularJS', 1),
+-- (7, 'Node.js', 1),
+-- (8, 'Bootstrap', 1),
+-- (9, 'WebApp', 1),
+-- (10, '前端工具', 1),
+-- (11, 'PHP', 2),
+-- (12, 'JAVA', 2),
+-- (13, 'Linux', 2),
+-- (14, 'Python', 2),
+-- (15, 'C', 2),
+-- (16, 'C++', 2),
+-- (17, 'Go', 2),
+-- (18, 'C#', 2),
+-- (19, '数据结构', 2);
 
 -- --------------------------------------------------------
 
@@ -105,14 +105,14 @@ CREATE TABLE IF NOT EXISTS `ci_course` (
 -- 转存表中的数据 `ci_course`
 --
 
-INSERT INTO `ci_course` (`course_id`, `course_name`, `course_synopsis`, `classify_id`, `course_lectruer_id`, `course_level`, `attentions`, `enrolls`, `img_path`) VALUES
-(1, 'PHP', '0', 0, 0, '', 0, 0, ''),
-(2, 'HTML+CSS基础课', '本课程从最基本的概念开始讲起，步步深入，带领大家学习HTML、CSS样式基础知识，了解各种常用标签的意义以及基本用法，后半部分讲解CSS样式代码添加，为后面的案例课程打下基础。', 1, 0, '初级', 0, 0, 'http://img.imooc.com/529dc3380001379906000338-240-135.jpg'),
-(3, '网页布局基础', '网页布局是进行网页制作的基础。本课程将讲解CSS中三种定位机制——标准文档流、浮动和绝对定位，并对标准文档流、盒子模型、float属性以及position属性等进行详细分析。从晦涩的理论讲解到编辑器环境的逐步验证，让你彻底掌握网页布局的相关知识。', 1, 0, '初级', 0, 0, 'http://img.imooc.com/53eafb44000146c706000338-240-135.jpg'),
-(4, '手把手教你实现电商网站', '课程介绍\r\n电商网站基本制作流程，通过整站分步的教学让学员了解和掌握电商网站制作的流程和注意事项，运用网站内学过的知识点的连接掌握整站的开发过程，增加开发经验。', 1, 0, '高级', 0, 0, 'http://img.imooc.com/53c4bf8200011aac06000338-240-135.jpg'),
-(5, '如何用CSS进行网页布', '如何用CSS进行网页布局？这可是前端工程师最最基本的技能，本课程教你怎么制作一列布局、二列布局、三列布局当然还有最通用的混合布局，而且你还可以选择让它固定还是自适应。用CSS重新规划你的网页，让你的网页从此更美观、更友好。', 1, 0, '中级', 0, 0, 'http://img.imooc.com/53eafb7a0001828906000338-240-135.jpg'),
-(6, 'JjavaScript', '本课程让您快速认识JavaScript，熟悉基本语法、窗口交互方法和通过DOM进行网页元素的操作，学会如何编写JS代码，如何运用JavaScript去操作HTML元素和CSS样式，为JavaScript深入学习打下基础。', 2, 0, '初级', 0, 0, 'http://img.imooc.com/53e1d0470001ad1e06000338-240-135.jpg'),
-(7, 'impress让你的内', '本课程讲解一个功能强大的制作演示文稿的工具，他叫做impress.js，如果你已经厌烦了PPT，那么不妨开始学习使用impress.js，他将会使你随便设置几个参数就能实现效果炫酷吊炸天的演示文稿', 2, 0, '初级', 0, 0, 'http://img.imooc.com/5386fece00016e0006000338-240-135.jpg');
+-- INSERT INTO `ci_course` (`course_id`, `course_name`, `course_synopsis`, `classify_id`, `course_lectruer_id`, `course_level`, `attentions`, `enrolls`, `img_path`) VALUES
+-- (1, 'PHP', '0', 0, 0, '', 0, 0, ''),
+-- (2, 'HTML+CSS基础课', '本课程从最基本的概念开始讲起，步步深入，带领大家学习HTML、CSS样式基础知识，了解各种常用标签的意义以及基本用法，后半部分讲解CSS样式代码添加，为后面的案例课程打下基础。', 1, 0, '初级', 0, 0, 'http://img.imooc.com/529dc3380001379906000338-240-135.jpg'),
+-- (3, '网页布局基础', '网页布局是进行网页制作的基础。本课程将讲解CSS中三种定位机制——标准文档流、浮动和绝对定位，并对标准文档流、盒子模型、float属性以及position属性等进行详细分析。从晦涩的理论讲解到编辑器环境的逐步验证，让你彻底掌握网页布局的相关知识。', 1, 0, '初级', 0, 0, 'http://img.imooc.com/53eafb44000146c706000338-240-135.jpg'),
+-- (4, '手把手教你实现电商网站', '课程介绍\r\n电商网站基本制作流程，通过整站分步的教学让学员了解和掌握电商网站制作的流程和注意事项，运用网站内学过的知识点的连接掌握整站的开发过程，增加开发经验。', 1, 0, '高级', 0, 0, 'http://img.imooc.com/53c4bf8200011aac06000338-240-135.jpg'),
+-- (5, '如何用CSS进行网页布', '如何用CSS进行网页布局？这可是前端工程师最最基本的技能，本课程教你怎么制作一列布局、二列布局、三列布局当然还有最通用的混合布局，而且你还可以选择让它固定还是自适应。用CSS重新规划你的网页，让你的网页从此更美观、更友好。', 1, 0, '中级', 0, 0, 'http://img.imooc.com/53eafb7a0001828906000338-240-135.jpg'),
+-- (6, 'JjavaScript', '本课程让您快速认识JavaScript，熟悉基本语法、窗口交互方法和通过DOM进行网页元素的操作，学会如何编写JS代码，如何运用JavaScript去操作HTML元素和CSS样式，为JavaScript深入学习打下基础。', 2, 0, '初级', 0, 0, 'http://img.imooc.com/53e1d0470001ad1e06000338-240-135.jpg'),
+-- (7, 'impress让你的内', '本课程讲解一个功能强大的制作演示文稿的工具，他叫做impress.js，如果你已经厌烦了PPT，那么不妨开始学习使用impress.js，他将会使你随便设置几个参数就能实现效果炫酷吊炸天的演示文稿', 2, 0, '初级', 0, 0, 'http://img.imooc.com/5386fece00016e0006000338-240-135.jpg');
 
 -- --------------------------------------------------------
 
@@ -130,12 +130,12 @@ CREATE TABLE IF NOT EXISTS `ci_direction` (
 -- 转存表中的数据 `ci_direction`
 --
 
-INSERT INTO `ci_direction` (`direction_id`, `direction_name`) VALUES
-(1, '前端开发'),
-(2, '后端开发'),
-(3, '移动开发'),
-(4, '数据处理'),
-(5, '图像处理');
+-- INSERT INTO `ci_direction` (`direction_id`, `direction_name`) VALUES
+-- (1, '前端开发'),
+-- (2, '后端开发'),
+-- (3, '移动开发'),
+-- (4, '数据处理'),
+-- (5, '图像处理');
 
 -- --------------------------------------------------------
 
@@ -213,13 +213,13 @@ CREATE TABLE IF NOT EXISTS `ci_section` (
 -- 转存表中的数据 `ci_section`
 --
 
-INSERT INTO `ci_section` (`section_id`, `section_name`, `create_time`, `creater`, `chapter_id`, `order_no`, `section_path`) VALUES
-(1, 'HTML5特性简介', '2015-12-01 00:00:00', '谭旭', 1, 1, ''),
-(2, 'HTML5元素、属性和格式化', '2015-12-01 04:12:09', '谭旭', 1, 2, ''),
-(3, 'JavaScript基础教程 ', '2015-12-01 04:12:09', '谭旭', 2, 1, 'http://code32.b0.upaiyun.com/video/Taylor%20Swift%20-%20Shake%20It%20Off.mp4'),
-(4, 'JavaScript语法详解', '2015-12-03 07:00:00', '谭旭', 2, 2, 'http://code32.b0.upaiyun.com/video/Taylor%20Swift%20-%20Shake%20It%20Off.mp4'),
-(5, 'HTML5样式、链接和表格', '2015-12-01 00:00:00', '冉子文', 1, 5, ''),
-(6, '代码初体验，制作我的第一个网页', '2016-01-01 00:00:00', '谭旭', 3, 1, '');
+-- INSERT INTO `ci_section` (`section_id`, `section_name`, `create_time`, `creater`, `chapter_id`, `order_no`, `section_path`) VALUES
+-- (1, 'HTML5特性简介', '2015-12-01 00:00:00', '谭旭', 1, 1, ''),
+-- (2, 'HTML5元素、属性和格式化', '2015-12-01 04:12:09', '谭旭', 1, 2, ''),
+-- (3, 'JavaScript基础教程 ', '2015-12-01 04:12:09', '谭旭', 2, 1, 'http://code32.b0.upaiyun.com/video/Taylor%20Swift%20-%20Shake%20It%20Off.mp4'),
+-- (4, 'JavaScript语法详解', '2015-12-03 07:00:00', '谭旭', 2, 2, 'http://code32.b0.upaiyun.com/video/Taylor%20Swift%20-%20Shake%20It%20Off.mp4'),
+-- (5, 'HTML5样式、链接和表格', '2015-12-01 00:00:00', '冉子文', 1, 5, ''),
+-- (6, '代码初体验，制作我的第一个网页', '2016-01-01 00:00:00', '谭旭', 3, 1, '');
 
 -- --------------------------------------------------------
 
