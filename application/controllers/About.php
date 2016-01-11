676f76e88da444c2c77b4cbb9bd890169e2d43ee
tanxu <773184719@qq.com>
2016-01-11 16:17:47 +0800

    add

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -17,5 +17,64 @@ class About extends CI_Controller {
 		$this->load->view("about.html");
 
 	}
+	public function us()
+	{
+
+		$this->load->helper('url');
+		$this->load->view("us.html");
+
+	}
+	public function group()
+	{
+
+		$this->load->helper('url');
+		$this->load->view("group.html");
+
+	}
+	public function job()
+	{
+
+		$this->load->helper('url');
+		$this->load->view("job.html");
+
+	}
+
+	public function feedback()
+	{
+
+		$this->load->helper('url');
+		$this->load->view("feedback.html");
+
+	}
+	public function friendly()
+	{
+
+		$this->load->helper('url');
+		$this->load->view("friendly.html");
+
+	}	
+	public function faq()
+	{
+
+		$this->load->helper('url');
+		$this->load->view("faq.html");
+
+	}	
+	public function contact()
+	{
+
+		$this->load->helper('url');
+		$this->load->view("contact.html");
+
+	}	
+	public function recruit()
+	{
+
+		$this->load->helper('url');
+		$this->load->view("recruit.html");
+
+	}
+
+
 
 }
