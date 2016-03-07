38ac4e057091aea0e2212b4b250568809274cb18
tanxu <773184719@qq.com>
2016-03-07 22:26:15 +0800

    add

--- application/controllers/Course.php
+++ application/controllers/Course.php
@@ -16,16 +16,20 @@ class Course extends CI_Controller {
 	{
 		$this->load->helper('url');
 		$course=$this->course_model->getcoursebyid($course_id);
-		//print_r($course);
-		//echo '<br>';
-		$chapter=$this->show_model->showchapterbyid($course_id);
-		$section=$this->show_model->showsectionbyid($course_id);
-	$course[0]['order_no']=$this->show_model->getsection_orderbyid($course_id)[0]['order_no'];
-	//print_r($course);
-		$data['arr']=array($chapter,$section,$course);
-		$this->session->set_userdata('course_id',$course_id);
-		$this->load->view("index/course.html",$data);
-		$this->load->view("about_footer.html");
+		if (empty($course)) {
+			$this->load->view("404.html");
+		}else{
+			//print_r($course);
+			//echo '<br>';
+			$chapter=$this->show_model->showchapterbyid($course_id);
+			$section=$this->show_model->showsectionbyid($course_id);
+			$course[0]['order_no']=$this->show_model->getsection_orderbyid($course_id)[0]['order_no'];
+			//print_r($course);
+			$data['arr']=array($chapter,$section,$course);
+			$this->session->set_userdata('course_id',$course_id);
+			$this->load->view("index/course.html",$data);
+			$this->load->view("about_footer.html");
+		}
 
 	}
 	function showinserttocourse(){
